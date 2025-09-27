0181638b220a2b8369619fb5cdd82e3960886632
kindabit <kindabit@saya.pw>
2025-09-27 11:55:13 +0800

    chore: erase all code

--- src/database/database.rs
+++
@@ -1,6 +0,0 @@
-/// A collection of plain (unencrypted) data of accounts and other necessary auxiliary data,
-/// which will be encrypted before being save to filesystem,
-/// and be decrypted to after being read from filesystem.
-pub struct Database {
-
-}
